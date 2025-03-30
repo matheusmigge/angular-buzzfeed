24077e6bdb3815c8ba7c1fd74d57b1c9d0bd33c9
Matheus Migge <matheusmigge@gmail.com>
2025-03-30 00:29:41 -0300

    Refactor quizz component: update styles, bind title and question, and implement question navigation

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import quizz_questions from "../../../../public/assets/data/quizz_question.json";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quizz',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
 
+  title: string = ""
+
+  questions: any
+  questionSelected: any
+
+  answers: string[] = []
+  answersSelected: string = ""
+
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished: boolean = false
+
+  constructor() {
+    if (quizz_questions) {
+      this.finished = false
+      this.title = quizz_questions.title
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value: string) {
+    this.answers.push(value)
+    this.nextStep()
+  }
+
+  nextStep() {
+    this.questionIndex += 1
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
+  }
 }
